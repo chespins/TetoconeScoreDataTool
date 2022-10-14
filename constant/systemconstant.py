6b414719d81525c988e50c66b70851979593a230
Chespins <sync.chespins@gmail.com>
2022-10-14 19:39:21 +0900

    URLパラメータ追加

--- constant/systemconstant.py
+++ constant/systemconstant.py
@@ -9,7 +9,7 @@ HTTP_ACCESS_URL = "https://" + HOST_URL + "/mypage-web2"
 WEB_LOGIN_URL = HTTP_ACCESS_URL + "/login"
 WEB_LOGINED_URL = HTTP_ACCESS_URL + "/?news=1"
 API_LOGIN_URL = HTTP_ACCESS_URL + "/api/login"
-DATA_GET_URL = HTTP_ACCESS_URL + "/api/user?data=stages"
+DATA_GET_URL = HTTP_ACCESS_URL + "/api/user?data=stages&lang=ja_jp"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
 
 # クリア状況確認用
