e4a402844a8b5bc193fbd640ef9f68de9b668f9e
Chespins <sync.chespins@gmail.com>
2022-12-18 14:25:20 +0900

    ファイル配置場所の整理

--- constant/systemconstant.py
+++ constant/systemconstant.py
@@ -10,7 +10,12 @@ WEB_LOGIN_URL = HTTP_ACCESS_URL + "/login"
 WEB_LOGINED_URL = HTTP_ACCESS_URL + "/?news=1"
 API_LOGIN_URL = HTTP_ACCESS_URL + "/api/login"
 DATA_GET_URL = HTTP_ACCESS_URL + "/api/user?data=stages&lang=ja_jp"
+
+# APIパラメータ
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+DNT = "1"
+ACCEPT_LANGUAGE = "ja"
+CONTENT_TYPE = "application/json"
 
 # クリア状況確認用
 FULL_COMBO = "フルコンボ"
@@ -27,3 +32,9 @@ CURRENT_DB_VERSION = "v0.5"
 DB_SUCCESS = 0
 DB_ERROR_FILE_BREAK = -1
 DB_ERROR_UNKNOWN_FILE = -2
+
+# ディレクトリ定義
+LIBRARY_LICENSE_DIR = "./lib_license/"
+DDL_DIR = "./ddl/"
+FONT_DIR = "./fonts"
+FONT_FILE_NAME = "Corporate-Mincho-ver2.otf"
--- model/mypagedata.py
+++ model/mypagedata.py
@@ -10,17 +10,17 @@ from constant import systemconstant as cons
 def getConnectPageData(cardId, password):
     session = requests.session()
     session.headers["user-agent"] = cons.USER_AGENT
-    session.headers["Accept-Language"] = "ja"
-    session.get(cons.WEB_LOGIN_URL)
+    session.headers["Accept-Language"] = cons.ACCEPT_LANGUAGE
+    loginPage = session.get(cons.WEB_LOGIN_URL)
     sleep(10)
     login_request_data = json.dumps({
             "card_id": cardId,
             "password": password
         })
-    session.headers["DNT"] = "1"
+    session.headers["DNT"] = cons.DNT
     session.headers["Host"] = cons.HOST_URL
     session.headers["Referer"] = cons.WEB_LOGIN_URL
-    session.headers["Content-Type"] = "application/json"
+    session.headers["Content-Type"] = cons.CONTENT_TYPE
     session.headers["Content-Length"] = str(len(login_request_data))
     login = session.post(cons.API_LOGIN_URL, login_request_data)
     if login.status_code != 200:
